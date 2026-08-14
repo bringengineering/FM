9fe3d802ff2d354fd207f092bc4dbf40ca544079
ameejin92-stack <ameejin92@gmail.com>
2026-08-14 12:33:00 +0900

    chore(release): prepare CRM 1.7.4

--- desktop-crm/test/company-release.test.js
+++ desktop-crm/test/company-release.test.js
@@ -4,7 +4,7 @@ const test = require("node:test");
 const packageJson = require("../package.json");
 
 test("builds a clearly named company-server installer without auto-publishing", () => {
-  assert.equal(packageJson.version, "1.7.3");
+  assert.equal(packageJson.version, "1.7.4");
   assert.equal(packageJson.build.artifactName, "BRING.CRM.Company.Setup.${version}.${ext}");
   assert.equal(packageJson.build.appId, "kr.co.bringengineering.crm");
 });
